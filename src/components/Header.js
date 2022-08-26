624c0341b0a669045a1c160b8ac6ddc1df8e1769
Camila Wolter <camila0wolter@gmail.com>
2022-08-26 18:42:38 -0300

    requisito 2

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,22 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 
 class Header extends Component {
   render() {
+    const { emailUser, total } = this.props;
     return (
-      <div>Header</div>
+      <header>
+        <p data-testid="email-field">{emailUser}</p>
+        <p data-testid="total-field">{total}</p>
+        <p data-testid="header-currency-field">BRL</p>
+      </header>
     );
   }
 }
+
+Header.propTypes = {
+  emailUser: PropTypes.string.isRequired,
+  total: PropTypes.number.isRequired,
+};
 
 export default Header;
--- src/pages/Wallet.js
+++ src/pages/Wallet.js
@@ -1,9 +1,35 @@
 import React from 'react';
+import PropTypes from 'prop-types';
+import { connect } from 'react-redux';
+import Header from '../components/Header';
 
 class Wallet extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      total: 0,
+    };
+  }
+
   render() {
-    return <div>TrybeWallet</div>;
+    const { emailUser } = this.props;
+    const { total } = this.state;
+    return (
+      <div>
+        <Header emailUser={ emailUser } total={ total } />
+        TrybeWallet
+      </div>
+    );
   }
 }
 
-export default Wallet;
+Wallet.propTypes = {
+  emailUser: PropTypes.string.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  emailUser: state.user.email,
+});
+
+export default connect(mapStateToProps, null)(Wallet);
